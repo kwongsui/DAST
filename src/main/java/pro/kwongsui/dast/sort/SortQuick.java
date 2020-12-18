9017159c4108ccc13a67a766197e6feb1fa3cddf
kwongsui <hwangkwongsui@gmail.com>
2020-12-19 04:47:54 +0800

    UPDATE：修正错误的注释

--- src/main/java/pro/kwongsui/dast/sort/SortQuick.java
+++ src/main/java/pro/kwongsui/dast/sort/SortQuick.java
@@ -17,7 +17,7 @@ public class SortQuick {
     int pivot = arr[right];
     // i把arr分成两部分，arr[left…i-1]元素小于pivot，是"已处理区间"，A[i…right-1]是"未处理区间"
     int i = left;
-    // 从未处理的区间arr[i…right-1]中取一个元素arr[j]，如果小于pivot，则加入到已处理区间尾部，即arr[i]的位置
+    // 从未处理区间arr[i…right-1]中取一个元素arr[j]，小于pivot，则加入到已处理区间尾部，即arr[i - 1]的位置
     for (int j = i; j < right; j++)
       if (arr[j] < pivot && i++ != j) { // i++说明已处理区间又多一个数据，i == j说明数据在已处理区间尾部
         int temp = arr[i - 1]; // i != j 则将arr[j]加入到已处理区间尾部arr[i-1]
